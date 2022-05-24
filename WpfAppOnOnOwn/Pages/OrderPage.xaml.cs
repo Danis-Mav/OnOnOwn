47cc3412924f0050859810f9927438b4abbac3fe
Danis Mavlin <mavlindanis@gmail.com>
2022-05-24 18:30:11 +0300

    проблема с type

--- WpfAppOnOnOwn/Pages/OrderPage.xaml.cs
+++ WpfAppOnOnOwn/Pages/OrderPage.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using OnOnOwn;
 
 namespace WpfAppOnOnOwn.Pages
 {
@@ -20,9 +22,18 @@ namespace WpfAppOnOnOwn.Pages
     /// </summary>
     public partial class OrderPage : Page
     {
+        public static ObservableCollection<OrderMenu> OrderMenu { get; set; }
+        public static ObservableCollection<menu> menu { get; set; }
+        //public static ObservableCollection<Type> Type { get; set; }
         public OrderPage()
         {
             InitializeComponent();
+            OrderMenu = new ObservableCollection<OrderMenu>(DBConnection.connection.OrderMenu.ToList());
+            this.DataContext = this;
+            menu = new ObservableCollection<menu>(DBConnection.connection.menu.ToList());
+            this.DataContext = this;
+            //Type = new ObservableCollection<Type>(DBConnection.connection.Type.ToList());
+            //this.DataContext = this;
         }
     }
 }
